6a8965cd5579631a8549ef2837dd4734ab87e4ba
128015116-spec <128015116@sastra.ac.in>
2026-07-23 23:35:51 +0530

    basic landing page in frontend done

---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>SwipeShop AI++</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/layout/Navbar";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Landing Page</h1>
+    </>
+  );
+};
+
+export default Landing;
